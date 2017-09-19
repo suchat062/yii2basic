85bb2e0ceb98985cfef40bccfc6c92d0024c7260
suchat <suchat062.rungchaeng@gmail.com>
2017-09-20 00:47:23 +0700

    init

---
+++ controllers/SiteController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+use linslin\yii2\curl\Curl;
+
+class SiteController extends Controller {
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions() {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public $enableCsrfValidation = false;
+    public function actionIndex() {
+        //return $this->render('index');
+
+
+        $curl = new Curl();
+        $response = $curl->setPostParams([
+            'key' => 'value',
+            'secondKey' => 'secondValue'
+        ])
+            ->setHeaders([
+                'Content-Type' => 'application/json; charset=UTF-8',
+            ])
+            ->post('http://localhost/yii2basic/web/index.php?r=site/sendmessage');
+        print_r($response);
+
+    }
+
+    public function actionSendmessage(){
+        echo "<pre>";
+        $strRequest = file_get_contents('php://input');
+        echo $strRequest;
+        //print_r(Yii::$app->request->getQueryString());
+//        print_r($strRequest = file_get_contents('php://input'));
+        echo "</pre>";
+//        return Yii::$app->response->statusCode = 200;
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin() {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+                    'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout() {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact() {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+                    'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout() {
+        return $this->render('about');
+    }
+
+}
